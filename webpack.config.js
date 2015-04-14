c649ff5c5eebc4083324bc1c60a9f34e733aa7f4
Ovidiu Cherecheș <skidding@gmail.com>
2015-04-14 21:22:55 +0300

    React can be required in more ways #1

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: './src/components/component-playground.jsx',
   externals: {
     'lodash': 'lodash',
+    'react': 'react',
     'react/addons': 'react/addons'
   },
   module: {
