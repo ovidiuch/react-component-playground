ed2359a55a53c5c33a28a3547fe7dfdfa51f2c95
Ovidiu Chereches <skidding@gmail.com>
2015-06-07 21:00:52 +0300

    Fake components yield weird behavior #15

--- fixtures/component-playground/default.js
+++ fixtures/component-playground/default.js
@@ -1,9 +1,21 @@
 var React = require('react');
 
+class FirstComponent extends React.Component {
+  render() {
+    return React.DOM.div();
+  }
+}
+
+class SecondComponent extends React.Component {
+  render() {
+    return React.DOM.div();
+  }
+}
+
 module.exports = {
   components: {
     FirstComponent: {
-      class: 'FirstComponent',
+      class: FirstComponent,
       fixtures: {
         'default': {
           myProp: false,
@@ -24,7 +36,7 @@ module.exports = {
       }
     },
     SecondComponent: {
-      class: 'SecondComponent',
+      class: SecondComponent,
       fixtures: {
         'index': {
           myProp: true,
